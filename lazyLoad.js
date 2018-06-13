5595dbe28fcf7d16b4e2947196766c48fd8482b1
wu_ze_qing <wuzeqing@zuzuche.com>
2018-06-13 15:05:13 +0800

    添加参数

--- lazyLoad.js
+++ lazyLoad.js
@@ -1,35 +1,47 @@
 /**
  *
  *
- * @param {*} option {
+ * @param {object} option {
                         selector: '[data-src]', //querySelectorAll选择元素的标识符
                         prefix: 'data-src', //存储图片src的data属性
                         scrollDelay: 25, //滚动函数执行间隔时间
                         resizeDelay: 50, //执行resize函数间隔时间
+                        distance: 0, //加载距离
+                        containerClass: '', //容器类名
                         container: '' //父容器
+                        loadingClassName: 'loading', //图片loading类名
+                        loadedClassName: 'loaded', //图片加载完成
+                        preloadImage: null, //预加载图片链接,
+                        rectScale: 1, //页面缩放比例
  * }
+ * @param {function} callback 处理所有图片的回调
+ * @param {function} errorFn 图片加载错误回调
  */
 
-function LazyLoad(option) {
+function LazyLoad(option, callback, errorFn) {
     var self = this;
 
     var util = self._util = {};
-    util.elements = [];
-    util.loaded = false;
-    util.scrollFn = null;
-    util.resizeFn = null;
+    util.elements  = [];
+    util.loaded    = false;
+    util.scrollFn  = null;
+    util.resizeFn  = null;
+    util.callback  = callback;
+    util.errorFn   = errorFn;
+    util.count     = 0; //统计所有的图片元素
     
-    self.option = option || {};
-    self.option.selector = self.option.selector || '[data-src]'; //querySelectorAll选择元素的标识符
-    self.option.prefix = self.option.prefix || 'data-src';  //存储图片src的data属性
-    self.option.scrollDelay = self.option.scrollDelay || 25; //滚动函数执行间隔时间
-    self.option.resizeDelay = self.option.resizeDelay || 50; //执行resize函数间隔时间
-    self.option.distance = self.option.distance || 0; //开始加载图片的距离
-    self.option.containerClass = self.option.containerClass || null; //容器类名
-    self.option.container = self.option.containerClass?document.querySelector(self.option.containerClass):null; //容器dom节点
+    self.option                  = option || {};
+    self.option.selector         = self.option.selector || '[data-src]'; //querySelectorAll选择元素的标识符
+    self.option.prefix           = self.option.prefix || 'data-src';  //存储图片src的data属性
+    self.option.scrollDelay      = self.option.scrollDelay || 25; //滚动函数执行间隔时间
+    self.option.resizeDelay      = self.option.resizeDelay || 50; //执行resize函数间隔时间
+    self.option.distance         = self.option.distance || 0; //开始加载图片的距离
+    self.option.containerClass   = self.option.containerClass || null; //容器类名
+    self.option.container        = self.option.containerClass?document.querySelector(self.option.containerClass): null; //容器dom节点
     self.option.loadingClassName = self.option.loadingClassName || 'loading'; //图片加载中样式
-    self.option.loadedClassName = self.option.loadedClassName || 'loaded'; //图片加载完成样式
-    self.option.preloadImage = self.option.preloadImage || null;
+    self.option.loadedClassName  = self.option.loadedClassName || 'loaded'; //图片加载完成样式
+    self.option.preloadImage     = self.option.preloadImage || null;
+    self.option.rectScale        = self.option.rectScale || 1;
 
     self._viewport = {
         bottom: window.innerHeight + self.option.distance,
@@ -49,9 +61,9 @@ function LazyLoad(option) {
 LazyLoad.prototype.init = function() {
     var self = this;
     var util = self._util;
-    util.elements = self.toArray();
 
-
+    util.elements = self.toArray(); //dom类数组转数组
+    util.count = util.elements.length;
     self.initPreloadImage();
 
     self.scroll(); //加载首屏图片
@@ -61,10 +73,12 @@ LazyLoad.prototype.init = function() {
     if(self.option.container) {
         self.bindEvent(self.option.container, 'scroll', util.scrollFn);
         self.bindEvent(self.option.container, 'resize', util.resizeFn);
+        self.bindEvent(self.option.container, 'resize', util.scrollFn);
     }
 
     self.bindEvent(window, 'scroll', util.scrollFn);
     self.bindEvent(window, 'resize', util.resizeFn);
+    self.bindEvent(window, 'resize', util.scrollFn);
 }
 
 /**
@@ -79,10 +93,12 @@ LazyLoad.prototype.destroy = function() {
     if(self.option.container) {
         self.unbindEvent(self.option.container, 'scroll', util.scrollFn);
         self.unbindEvent(self.option.container, 'resize', util.resizeFn);
+        self.unbindEvent(self.option.container, 'resize', util.scrollFn);
     }
 
     self.unbindEvent(window, 'scroll', util.scrollFn);
     self.unbindEvent(window, 'resize', util.resizeFn);
+    self.unbindEvent(window, 'resize', util.scrollFn);
 }
 
 /**
@@ -146,14 +162,28 @@ LazyLoad.prototype.scroll = function() {
         let item = util.elements[i];
         let result = self.checkInView(item, self._viewport); //检测是否在页面中
         if(result) {
+            console.log('true')
             let srcLink = item.getAttribute(self.option.prefix);
             let image = new Image();
             image.src = srcLink;
+
             image.onload = function() {
                 item.src = srcLink;
                 item.classList.remove(self.option.loadingClassName);
                 item.classList.add(self.option.loadedClassName);
+                if(--util.count <= 0) {
+                    if(util.callback) {
+                        util.callback();
+                    }
+                }
             }
+
+            image.onerror = function() {
+                if(util.errorFn) {
+                    util.errorFn();
+                }
+            }
+
             util.elements.splice(i, 1);
 
             //手动减少长度
@@ -217,12 +247,20 @@ LazyLoad.prototype.toArray = function() {
  * @param {object} viewport 坐标信息对象
  */
 LazyLoad.prototype.checkInView = function(ele, viewport) {
+    let self = this;
     let rect = ele.getBoundingClientRect();
+    console.log('rect: ', rect);
+    rect.right = rect.right*self.option.rectScale;
+    rect.bottom = rect.bottom*self.option.rectScale;
+    rect.left = rect.left*self.option.rectScale;
+    rect.top = rect.top*self.option.rectScale;
+    console.log('rect: ', rect);
+    console.log('viewport: ', viewport);
 
-    return rect.right >= viewport.left &&
-        rect.bottom >= viewport.top && 
-        rect.left <= viewport.right && 
-        rect.top <= viewport.bottom;
+    return rect.right*self.option.rectScale >= viewport.left &&
+        rect.bottom*self.option.rectScale >= viewport.top && 
+        rect.left*self.option.rectScale <= viewport.right && 
+        rect.top*self.option.rectScale <= viewport.bottom;
 }
 
 /**
@@ -243,8 +281,3 @@ function throttle(fn, minDelay, scope) {
         fn.apply(scope, arguments);
     };
 }
-
-window.test = new LazyLoad({
-    containerClass: '.wrap',
-    preloadImage: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif'
-});
